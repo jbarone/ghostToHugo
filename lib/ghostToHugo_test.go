9898f564b778ab8f8394bda3d555c161d3c6c31c
Joshua Barone <joshua.barone@gmail.com>
2016-11-27 12:50:32 -0600

    better import processing
    reduced to only 2 passes.

--- lib/ghostToHugo_test.go
+++ lib/ghostToHugo_test.go
@@ -1,34 +1,64 @@
 package ghostToHugo
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
 func TestImportGhost(t *testing.T) {
-	data := []struct {
-		Filename string
-	}{
-		{"wrapped.json"},
-		{"unwrapped.json"},
+	data := []string{
+		"wrapped.json",
+		"unwrapped.json",
 	}
 	for _, d := range data {
-		f, err := os.Open(filepath.Join("testdata", d.Filename))
+		f, err := os.Open(filepath.Join("testdata", d))
 		if err != nil {
-			t.Error(err) //TODO: better message
+			t.Error(err)
 		}
+
 		gth, _ := NewGhostToHugo()
 		posts, err := gth.importGhost(f)
 		if err != nil {
 			t.Error(err)
 		}
+
+		var entryCount int
 		for p := range posts {
-			t.Log(p)
+			entryCount++
+			if reflect.DeepEqual(expectedPost, p) {
+				t.Errorf("Expected: %v Actual: %v", expectedPost, p)
+			}
 		}
+
+		if entryCount != 1 {
+			t.Errorf("Expected 1 entry, found %v", entryCount)
+		}
+
 		err = f.Close()
 		if err != nil {
 			t.Error(err)
 		}
 	}
+}
+
+var expectedPost = Post{
+	ID:              5,
+	Title:           "my blog post title",
+	Slug:            "my-blog-post-title",
+	Content:         "the *markdown* formatted post body",
+	Image:           "",
+	Page:            json.RawMessage("0"),
+	Status:          "published",
+	MetaDescription: "",
+	AuthorID:        1,
+	PublishedAt:     json.RawMessage("1283780649000"),
+	CreatedAt:       json.RawMessage("1283780649000"),
+
+	IsDraft: false,
+	IsPage:  false,
+	Author:  "user's name",
+	Tags:    []string{"Colorado Ho!"},
 }
